a3018f9b6063a5e242a4d74e5ffb6c889915817e
sachins1995 <101519463+sachins1995@users.noreply.github.com>
2025-07-30 12:24:47 +0530

    Add files via upload

---
+++ homepage.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+LATEST_MODEL_RUN = 'Predicted Mid July'
+
+st.set_page_config(
+
+    layout="wide",
+)
+
+
+pages={"VAAP":[st.Page("dashboard_shrimp.py", title="Aqua"), st.Page("dashboard_coffee.py", title="Coffee"), st.Page("dashboard_chili.py", title="Chili")],
+        "OGP":[ 
+                    st.Page("dashboard_wheat.py", title="Wheat"),
+                    st.Page("dashboard_soya.py", title="Soya"),
+                    st.Page("dashboard_chana.py", title="Chana"),
+                    st.Page("dashboard_basmati.py", title="Basmati"),
+                    st.Page("dashboard_finepaddy.py", title="Fine Paddy"),
+                    st.Page("dashboard_maize.py", title="Maize")
+                    ]}
+pg = st.navigation(pages, position="top", expanded=False)
+pg.run()
